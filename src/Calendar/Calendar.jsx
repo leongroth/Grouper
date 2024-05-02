be3866b2ab0db4413bc89bf48b4a706d80f63987
leongroth <leongroth@gmail.com>
2024-05-02 11:32:46 +0200

    calendar test

--- src/Calendar/Calendar.jsx
+++ src/Calendar/Calendar.jsx
@@ -1,1 +1,11 @@
-//Calendar
+import {db} from '../config/firebase'
+
+function Calendar() {
+    const today = new Date()
+    const month = today.getMonth() +1
+    const year = today.getFullYear()
+    const date = today.getDate()
+    
+
+    return <div>{day} {month} {year}</div>
+}
--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -1,5 +1,6 @@
 
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBdx_hurbjgalcoZMNENB6H7mtKGc2bpHY",
@@ -11,3 +12,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
