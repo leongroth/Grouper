2e667acde98fb1cd788e2b560261848921024f4c
SnemandFrostHamSelv <104226829+SnemandFrostHamSelv@users.noreply.github.com>
2024-05-10 10:50:37 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,18 @@ function App() {
   return (
     <>
       <div>
+      <button>
+            <a href={'/login'}>login</a>
+        </button>
+        <button>
+            <a href={'/signup'}>register</a>
+        </button>
+        <button>
+            <a href="/Calendar">Calendar</a>
+        </button>
+        <button>
+            <a href="/Teamwall">Teamwall</a>
+        </button>
         <Outlet/>
       </div>
     </>
