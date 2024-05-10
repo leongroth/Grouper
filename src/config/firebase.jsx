997444a545ac6fd0a508d53206c9473af83d9703
SnemandFrostHamSelv <104226829+SnemandFrostHamSelv@users.noreply.github.com>
2024-05-10 15:39:01 +0200

    Update firebase.jsx

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -5,13 +5,21 @@ import {getAuth} from 'firebase/auth'
 
 const firebaseConfig = {
 
-  apiKey: "AIzaSyDgx-jxzxrD9B4NbJVYK8GUdHUQVEqxoxg",
-  authDomain: "nytester-de30d.firebaseapp.com",
-  projectId: "nytester-de30d",
-  storageBucket: "nytester-de30d.appspot.com",
-  messagingSenderId: "353756479601",
-  appId: "1:353756479601:web:7a95da090d6f60a9d39bd0"
-};
+
+    apiKey: "AIzaSyDgx-jxzxrD9B4NbJVYK8GUdHUQVEqxoxg",
+  
+    authDomain: "nytester-de30d.firebaseapp.com",
+  
+    projectId: "nytester-de30d",
+  
+    storageBucket: "nytester-de30d.appspot.com",
+  
+    messagingSenderId: "353756479601",
+  
+    appId: "1:353756479601:web:7a95da090d6f60a9d39bd0"
+  
+  };
+  
 //test
 
 const app = initializeApp(firebaseConfig);
