69335dc65567b0e3660923707b7df8522416a047
leongroth <leongroth@gmail.com>
2024-05-10 10:56:48 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,44 @@ function App() {
   return (
     <>
       <div>
-      <button>
-            <a href={'/login'}>login</a>
-        </button>
-        <button>
-            <a href={'/signup'}>register</a>
-        </button>
-        <button>
-            <a href="/Calendar">Calendar</a>
-        </button>
-        <button>
-            <a href="/Teamwall">Teamwall</a>
-        </button>
-        <Outlet/>
+      <table>
+        <th style={navbarStyle}>
+          <tr>
+            <button>
+                <a href={'/login'}>login</a>
+            </button>
+          </tr>
+          <tr>
+            <button>
+                <a href={'/signup'}>register</a>
+            </button>
+          </tr>
+          <tr>
+            <button>
+                <a href="/Calendar">Calendar</a>
+            </button>
+          </tr>
+          <tr>
+            <button>
+                <a href="/Teamwall">Teamwall</a>
+            </button>
+          </tr>
+        </th>
+        <th>
+          <Outlet/>
+        </th>
+      </table>
+
+        
+        
       </div>
     </>
   )
+}
+
+const navbarStyle = {
+  width: "200px",
+  background: "#C7C7C7"
 }
 
 export default App
