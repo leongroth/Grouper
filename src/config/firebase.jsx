e33da7c2d2a7b32f08148582ef4fedd1af2e15bc
leongroth <leongroth@gmail.com>
2024-05-14 12:28:39 +0200

    firebase change

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -1,27 +1,22 @@
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import {getDatabase} from 'firebase/database'
 import {getAuth} from 'firebase/auth'
 
 const firebaseConfig = {
-
-
   apiKey: "AIzaSyBdx_hurbjgalcoZMNENB6H7mtKGc2bpHY",
-
   authDomain: "login-auth-6e5a8.firebaseapp.com",
-
+  databaseURL: "https://login-auth-6e5a8-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "login-auth-6e5a8",
-
   storageBucket: "login-auth-6e5a8.appspot.com",
-
   messagingSenderId: "522872966180",
-
   appId: "1:522872966180:web:a17c74f9a05ee5bf7edcb1"
-  };
+};
   
 //test
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
-export const db = getFirestore(app)
+export const db = getDatabase(app)
 
