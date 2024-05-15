da3a5b206e563e4aebccdf2a24a1006f3eddf273
SnemandFrostHamSelv <mathias_frost_j@hotmail.com>
2024-05-15 12:19:49 +0200

    routerchange

--- src/PBL/PBL.jsx
+++ src/PBL/PBL.jsx
@@ -1,1 +1,9 @@
 //PBL
+
+
+export default function PBL(){
+    return(
+        <input type="button" value="Group conflicts" onClick={window.open('https://www.studerende.aau.dk/studieliv/studieteknik/inspiration-til-dit-gruppearbejde#')} />
+    )
+}
+https://www.studerende.aau.dk/studieliv/studieteknik/inspiration-til-dit-gruppearbejde#
--- src/router.jsx
+++ src/router.jsx
@@ -5,6 +5,7 @@ import Signup from './LoginPage/pages/Signup.jsx'
 import Home from "./LoginPage/pages/Home.jsx";
 import Tekstskriver from "./Team Wall/TeamWall.jsx";
 import { Calendar } from "./Calendar/Calendar.jsx";
+import PBL from "./PBL/PBL.jsx";
 
 export const router= createBrowserRouter (
     [
@@ -16,7 +17,8 @@ export const router= createBrowserRouter (
                 {path:"/login", element:<Login/>},
                 {path:"/signup", element:<Signup/>},
                 {path:"/teamwall", element:<Tekstskriver/>},
-                {path:"/Calendar", element:<Calendar />}
+                {path:"/Calendar", element:<Calendar />},
+                {path:"/PBL", element:<PBL/>},
             ]
         }
     ]
