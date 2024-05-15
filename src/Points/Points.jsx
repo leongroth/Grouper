52dcd981ca8494a83167d5f2ce9e49dd02f42bd4
leongroth <leongroth@gmail.com>
2024-05-15 13:25:57 +0200

    Points css

--- src/Points/Points.jsx
+++ src/Points/Points.jsx
@@ -1,16 +1,31 @@
 import { useState } from "react";
 import { onValue } from "firebase/database";
+import "./Points.css"
 
 export function PointsPage() {
     const [totalPoints, setTotalPoints] = useState(0)
+    const [sessionsMonth, setSessionsMonth] = useState(0)
 
     return (
         <div>
-            <div>
-                Total Points
-                <br></br>
-                {totalPoints}
-            </div>
+            <table>
+                <th>
+                    <div className="TP-Box">
+                    Total Points
+                    <br></br>
+                    {totalPoints}
+                    </div>
+                </th>
+                <th>
+                    <div className="M-Sesh">
+                    Sessions this month
+                    <br/>
+                    {sessionsMonth}
+                    </div>
+                </th>
+            </table>
+            
+            
         </div>
     )
 
