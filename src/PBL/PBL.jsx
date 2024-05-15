277a8a6f3785c832a33743de2c5e512152d474ea
SnemandFrostHamSelv <mathias_frost_j@hotmail.com>
2024-05-15 12:26:30 +0200

    Update PBL.jsx

--- src/PBL/PBL.jsx
+++ src/PBL/PBL.jsx
@@ -3,7 +3,6 @@
 
 export default function PBL(){
     return(
-        <input type="button" value="Group conflicts" onClick={window.open('https://www.studerende.aau.dk/studieliv/studieteknik/inspiration-til-dit-gruppearbejde#')} />
+        <input type="button" value="Group conflicts" onClick={ ()=> {window.open('https://www.studerende.aau.dk/studieliv/studieteknik/inspiration-til-dit-gruppearbejde#') }}/>
     )
 }
-https://www.studerende.aau.dk/studieliv/studieteknik/inspiration-til-dit-gruppearbejde#
