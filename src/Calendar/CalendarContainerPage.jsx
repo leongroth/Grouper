0883c7caa872c5e65e8a0e2b8730205b6d941768
leongroth <leongroth@gmail.com>
2024-05-16 09:45:48 +0200

    Calendar test

---
+++ src/Calendar/CalendarContainerPage.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export function CalendarContainerPage() {
+
+
+
+    return (
+        <div>
+            <div>
+                <h1>Test</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora ullam iusto inventore vel a in nulla quidem perspiciatis quo quos?</p>
+            </div>
+        </div>
+    )
+}
