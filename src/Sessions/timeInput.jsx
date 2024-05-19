32d9bad7ef77fe86b9de25ce18680a270a2914b4
sashanasivasothy <127312457+sashanasivasothy@users.noreply.github.com>
2024-05-19 23:13:31 +0200

    breaks hver 25 min

--- src/Sessions/timeInput.jsx
+++ src/Sessions/timeInput.jsx
@@ -91,7 +91,7 @@ const TimeInput = () => {
     if (countdownStarted && secondsRemaining > 0) {
       const totalSeconds = getSecondsFromHHMMSS(value);
       const minutesPassed = (totalSeconds - secondsRemaining) / 60;
-      if (minutesPassed % 0.25 === 0 && minutesPassed !== 0) { // checks if 25 minutes passed
+      if (minutesPassed % 25 === 0 && minutesPassed !== 0) { // checks if 25 minutes passed
         setShowBreakWheel(true);
         setIsToggled(true);
         setIsActive(false); // Pause the countdown
