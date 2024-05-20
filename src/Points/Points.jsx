32c09edf3d3b49f53adf2b87721e04d3b97c92b0
leongroth <leongroth@gmail.com>
2024-05-20 09:42:15 +0200

    Fin?

--- src/Points/Points.jsx
+++ src/Points/Points.jsx
@@ -1,11 +1,40 @@
 import { useState } from "react";
-import { onValue } from "firebase/database";
+import { get, onValue, ref } from "firebase/database";
+import {db} from '../config/firebase'
 import "./Points.css"
 
 export function PointsPage() {
     const [totalPoints, setTotalPoints] = useState(0)
     const [sessionsMonth, setSessionsMonth] = useState(0)
     const [sessionsDay, setSessionsDay] = useState(0)
+    const [keys, setKeys] = useState([])
+
+    const date = new Date()
+    const currentDate = `${date.getDate()}-${date.getMonth() +1}-${date.getFullYear()}`
+    const currentMonth = date.getMonth() + 1
+
+    const getPoints = () => {
+        const reference = ref(db, "Points/")
+        onValue(reference, (snapshot) => {
+            snapshot.forEach((childsnapshot) => {
+                const key = childsnapshot.key
+                const points = childsnapshot.val().points
+                const date = childsnapshot.val().day
+                const month = childsnapshot.val().month
+                if(keys.indexOf(key) == -1){
+                    setKeys(prevKey => [...prevKey, key])
+                    setTotalPoints(totalPoints => totalPoints + points)
+                    if(month == currentMonth){
+                        setSessionsMonth(sessionsMonth => sessionsMonth + 1)
+                    }
+                    if(date == currentDate){
+                        setSessionsDay(sessionsDay => sessionsDay + 1)
+                    }
+                }
+            })
+        })
+    }
+    getPoints()
 
     return (
         <div>
@@ -33,6 +62,7 @@ export function PointsPage() {
                     </div>
                 </th>
             </table>
+
         </div>
     )
 
